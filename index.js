1cc2b6da722d85532622c13220c3328582f9b7f4
stomg7969 <stomg7969@gmail.com>
2019-03-10 19:40:38 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file.
+
+function lowerCaseDrivers(drivers) {
+  return drivers.map(driver => driver.toLowerCase());
+}
+// function lowerCaseDrivers(drivers) {
+//   return drivers.map(function(driver) {
+//     return driver.toLowerCase();
+//   });
+// }
+// **********************************
+
+function nameToAttributes(drivers) {
+  return drivers.map(function(driver) {
+    const object = {
+      firstName: driver.split(' ')[0],
+      lastName: driver.split(' ')[1]
+    };
+    return object;
+  });
+}
+// **********************************
+
+function attributesToPhrase(drivers) {
+  // console.log(drivers);
+  return drivers.map(function(driver) {
+    return `${driver.name} is from ${driver.hometown}`;
+  });
+}
