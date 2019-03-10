b876b38654a86646fedb5f8ce5beb13bc702172d
github username <github email address>
2019-03-10 12:17:09 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file.
+
+function lowerCaseDrivers(drivers) {
+  let names = drivers.map(name => name.toLowerCase());
+  return names;
+}
+
+function nameToAttributes(drivers) {
+  let names = drivers.map( function(name) {return Object.assign({}, {firstName: (name.split(" "))[0], lastName: (name.split(" "))[1]})});
+  return names;
+}
+
+function attributesToPhrase(drivers) {
+  let result = drivers.map( function(driver) {return `${driver.name} is from ${driver.hometown}`});
+  return result;
+}
